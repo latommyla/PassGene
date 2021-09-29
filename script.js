41828de477183701f89bd536e24ac9432640efb9
LATOMMYLA <tommyl.dmd@gmail.com>
2021-09-29 14:39:56 -0700

    testing new changes on password input

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 // Assignment Code
 var enter;
 var options;
-var password = [];
-var generateBtn = document.querySelector("#generate");
 
 upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", ">", "<", "?", "/"];
 
+/*
 // Write password to the #password input
-function writePassword() {
+function writePassword () {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
 }
+*/
 
+var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", function () {
   ps = generatePassword();
-  document.getElementById("generate").placeholder = ps;
+  document.getElementById("password").placeholder = ps;
 });
 
 
@@ -92,14 +94,15 @@ function generatePassword(){
 
   var password = [];
 
+  // Random selection variable
   for (var i = 0; i < enter; i++) {
     var chooseOptions = options[Math.floor(Math.random() * options.length)];
-    passwordText.push(chooseOptions);
+    password.push(chooseOptions);
   }
 
-  var ps = password.join("");
-  UserInput(ps);
-  return ps;
+    var password = password.join("");
+    UserInput(ps);
+    return password;
   }
 
   function UserInput(ps) {
